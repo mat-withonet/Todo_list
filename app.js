cb70ec3ffb57293c31b3a39de950c17318c9b2d8
Mat <63187287+mat-withonet@users.noreply.github.com>
2021-01-24 22:32:45 +0200

    :)

--- app.js
+++ app.js
@@ -1,100 +1,86 @@
-//Mats todo list functions. 
-
-
+//Mats todo list functions.
 
 //selectors
-
 
 const todoInput = document.querySelector(".todo-input");
 const toDoButton = document.querySelector(".todo-button");
 const toDoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector("filter-todo");
-
+const filterOption = document.querySelector(".filter-todo");
 
 //event listeners
 
 toDoButton.addEventListener("click", addToTheTodoList);
 toDoList.addEventListener("click", deleteCheck);
-filterOption.addEventListener(("click", filterTodo))
+filterOption.addEventListener("click", filterTodo);
 
+//functions
 
-//functions 
-
-function addToTheTodoList(event){
-    event.preventDefault();
-   // Create a todo div which will contiain each line of the todo list. 
-   const toDoDiv = document.createElement("div");
-   toDoDiv.classList.add("todo")
-   // Create the list
-   const newTodo = document.createElement("li");
-    newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo-item');
-    toDoDiv.appendChild(newTodo);
-    // adding a completed button
-    const completedButton = document.createElement("button")
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
-    completedButton.classList.add("complete-btn");
-    toDoDiv.appendChild(completedButton);
-    // adds the trash button
-    const trashButton = document.createElement("button");
-    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add("trash-btn");
-    toDoDiv.appendChild(trashButton);
-    // add to the main list 
-    toDoList.appendChild(toDoDiv);
-    //Clear the input value
-    todoInput.value = ""
-} 
-
-function deleteCheck(e){
-    //target shows us what we are clicking on. 
-    //console.log(e.target);
-    const button = e.target;
-    if (button.classList[0] === "trash-btn"){
-        const todo = button.parentElement;
-        todo.classList.add("fall")
-        todo.addEventListener("transitionend", function(){
-            todo.remove();
-        });
-    }
-
-    //What happens when you get the Tick Button. 
-    if(button.classList[0]=== "complete-btn"){
-        const todo = button.parentElement;
-        // added the class "completed" to the "todo" div. 
-        todo.classList.toggle('completed')
-        console.log("some shit just got toggled")
-    }
+function addToTheTodoList(event) {
+  event.preventDefault();
+  // Create a todo div which will contiain each line of the todo list.
+  const toDoDiv = document.createElement("div");
+  toDoDiv.classList.add("todo");
+  // Create the list
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  toDoDiv.appendChild(newTodo);
+  // adding a completed button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  toDoDiv.appendChild(completedButton);
+  // adds the trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  toDoDiv.appendChild(trashButton);
+  // add to the main list
+  toDoList.appendChild(toDoDiv);
+  //Clear the input value
+  todoInput.value = "";
 }
 
-function getTodos() {
-    let todos;
-    if (localStorage.getItem("todos") === null) {
-      todos = [];
-    } else {
-      todos = JSON.parse(localStorage.getItem("todos"));
-    }
-    todos.forEach(function(todo) {
-      //Create todo div
-      const todoDiv = document.createElement("div");
-      todoDiv.classList.add("todo");
-      //Create list
-      const newTodo = document.createElement("li");
-      newTodo.innerText = todo;
-      newTodo.classList.add("todo-item");
-      todoDiv.appendChild(newTodo);
-      todoInput.value = "";
-      //Create Completed Button
-      const completedButton = document.createElement("button");
-      completedButton.innerHTML = `<i class="fas fa-check"></i>`;
-      completedButton.classList.add("complete-btn");
-      todoDiv.appendChild(completedButton);
-      //Create trash button
-      const trashButton = document.createElement("button");
-      trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
-      trashButton.classList.add("trash-btn");
-      todoDiv.appendChild(trashButton);
-      //attach final Todo
-      todoList.appendChild(todoDiv);
+function deleteCheck(e) {
+  //target shows us what we are clicking on.
+  //console.log(e.target);
+  const button = e.target;
+  if (button.classList[0] === "trash-btn") {
+    const todo = button.parentElement;
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
     });
   }
+  //What happens when you get the Tick Button.
+  if (button.classList[0] === "complete-btn") {
+    const todo = button.parentElement;
+    // added the class "completed" to the "todo" div.
+    todo.classList.toggle("completed");
+    console.log("some shit just got toggled");
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+    }
+  });
+}
